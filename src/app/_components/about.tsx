44399b93d378fce64aff7242073e2f1a0ae9f241
Xande-PtBr <xande.xas@gmail.com>
2025-08-26 16:17:08 +0100

    enviando arquivos para github

---
+++ src/app/_components/about.tsx
@@ -0,0 +1,9 @@
+export function About() {
+  return (
+    <section>
+
+      <h1>Sobre</h1>
+
+    </section>
+  )
+}
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,5 +1,11 @@
+import { About } from "./_components/about";
 import { Hero } from "./_components/hero";
 
 export default function Home() {
-  return <main><Hero /></main>;
+  return ( 
+    <main>
+      <Hero />
+      <About />
+    </main>
+  )
 }
